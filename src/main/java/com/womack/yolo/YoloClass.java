728a05d18af1842b543cbac2a301fc3a9609916f
Steve Womack <stwomack@gmail.com>
2025-03-02 22:03:38 -0600

    Updates

--- src/main/java/com/womack/yolo/YoloClass.java
+++ src/main/java/com/womack/yolo/YoloClass.java
@@ -1,6 +1,34 @@
 package com.womack.yolo;
 
+import org.openrewrite.*;
+
 public class YoloClass {
-    String thisString = "this is a string";
+    static String thisString = "this is a string";
     String otherString = "ThIS iS A sTrinG";
+
+    public void doStuff() {
+        SteveRecipe steveRecipe = new SteveRecipe();
+
+        if (steveRecipe instanceof Recipe) {
+            System.out.println("yep");
+        }
+    }
+}
+
+class SteveRecipe extends Recipe {
+    @Override
+    public String getDisplayName() {
+        return "Append to release notes";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adds the specified line to RELEASE.md.";
+    }
+
+    // The shared state between the scanner and the visitor. The custom class ensures we can easily extend the recipe.
+    public static class Accumulator {
+        boolean found;
+    }
+
 }
--- src/test/java/com/womack/UppercaseAllStringLiteralsTest.java
+++ src/test/java/com/womack/UppercaseAllStringLiteralsTest.java
@@ -66,5 +66,84 @@ class UppercaseAllStringLiteralsTest implements RewriteTest {
           )
         );
     }
+    @Test
+    void findInstanceOf() {
+        rewriteRun(
+          java(
+            """
+              package com.womack.yolo;
+              
+              import org.openrewrite.*;
+              
+              public class YoloClass {
+                  static String thisString = "this is a string";
+                  String otherString = "ThIS iS A sTrinG";
+              
+                  public void doStuff() {
+                      SteveRecipe steveRecipe = new SteveRecipe();
+              
+                      if (steveRecipe instanceof Recipe) {
+                          System.out.println("yep");
+                      }
+                  }
+              }
+              
+              class SteveRecipe extends Recipe {
+                  @Override
+                  public String getDisplayName() {
+                      return "Append to release notes";
+                  }
+              
+                  @Override
+                  public String getDescription() {
+                      return "Adds the specified line to RELEASE.md.";
+                  }
+              
+                  // The shared state between the scanner and the visitor. The custom class ensures we can easily extend the recipe.
+                  public static class Accumulator {
+                      boolean found;
+                  }
+                 \s
+              }
+              """,
+            """
+              package com.womack.yolo;
+              
+              import org.openrewrite.*;
+              
+              public class YoloClass {
+                  static String thisString = "this is a string";
+                  String otherString = "ThIS iS A sTrinG";
+              
+                  public void doStuff() {
+                      SteveRecipe steveRecipe = new SteveRecipe();
+              
+                      if (steveRecipe instanceof Recipe) {
+                          System.out.println("yep");
+                      }
+                  }
+              }
+              
+              class SteveRecipe extends Recipe {
+                  @Override
+                  public String getDisplayName() {
+                      return "Append to release notes";
+                  }
+              
+                  @Override
+                  public String getDescription() {
+                      return "Adds the specified line to RELEASE.md.";
+                  }
+              
+                  // The shared state between the scanner and the visitor. The custom class ensures we can easily extend the recipe.
+                  public static class Accumulator {
+                      boolean found;
+                  }
+                 \s
+              }
+              """
+          )
+        );
+    }
 
 }
